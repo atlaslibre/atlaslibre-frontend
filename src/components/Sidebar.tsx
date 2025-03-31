ccc6b8b5ddd6fb535b0b891d3ff7f6e37c6118e6
Christian Panton <christian@panton.org>
2025-03-31 21:25:37 +0200

    add link to github in sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -54,15 +54,29 @@ export default function Sidebar() {
         <StaticLayersMenu />
       </div>
 
-      <div className="p-4 ">
-      {debuggingEnabled && (
-        <div className="text-xs text-neutral-300 pb-4 text-right w-full">
-         <code>
-         <strong>Version {__APP_VERSION__}</strong>
-         <br />{__COMMIT_DATE__.trim()} commit {__COMMIT_HASH__}
-        </code>
-        </div>
-      )}
+      <div className="p-4">
+        {debuggingEnabled && (
+          <div className="text-xs text-neutral-300 pb-4 text-right w-full">
+            <div className="pb-2">
+              Please provide any bug reports <br />
+              or ideas on the backlog at{" "}
+              <a
+                href="https://github.com/atlaslibre/design/issues"
+                className="text-blue-200"
+                target="_blank"
+              >
+                Github
+              </a>
+            </div>
+            <code>
+              <strong>Version {__APP_VERSION__}</strong>
+              <br />
+              {__COMMIT_DATE__.trim()}
+              <br />
+              Commit: {__COMMIT_HASH__}
+            </code>
+          </div>
+        )}
 
         <Stack direction="row" spacing={1} justifyContent="flex-end">
           <DebuggingSwitch />
