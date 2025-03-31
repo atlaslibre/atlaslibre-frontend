22ddd56d497e30832681fcf2c779d0be34facf05
Christian Panton <christian@panton.org>
2025-03-31 20:59:58 +0200

    add military type vessels

--- src/components/layers/DeckGLLayers.tsx
+++ src/components/layers/DeckGLLayers.tsx
@@ -14,14 +14,48 @@ function DeckGLOverlay(props: MapboxOverlayProps) {
 export default function DeckGLLayers() {
   const actors = useAppSelector((state) => state.gossip.actors);
 
-  const aircraft = gossipLayer(actors.filter((s) => s.type == "aircraft"), "airplane.glb", 25, 0.1, 3);
+  const aircraft = gossipLayer(
+    actors.filter((s) => s.type == "aircraft"),
+    "airplane.glb",
+    25,
+    0.1,
+    3
+  );
 
-  const navigation = gossipLayer(actors.filter((s) => s.type == "ship" && s.class == "navigation"), "buoy.glb");
-  const tankers = gossipLayer(actors.filter((s) => s.type == "ship" && s.class == "tanker"), "tanker.glb", 15, 0.1, 1);
-  const cargo = gossipLayer(actors.filter((s) => s.type == "ship" && s.class == "cargo"), "cargo.glb", 15, 0.1, 1);
-  const container = gossipLayer(actors.filter((s) => s.type == "ship" && s.class == "container"), "container.glb", 15, 0.1, 1);
+  const navigation = gossipLayer(
+    actors.filter((s) => s.type == "ship" && s.class == "navigation"),
+    "buoy.glb"
+  );
+  const tankers = gossipLayer(
+    actors.filter((s) => s.type == "ship" && s.class == "tanker"),
+    "tanker.glb",
+    15,
+    0.1,
+    1
+  );
+  const cargo = gossipLayer(
+    actors.filter((s) => s.type == "ship" && s.class == "cargo"),
+    "cargo.glb",
+    15,
+    0.1,
+    1
+  );
+  const container = gossipLayer(
+    actors.filter((s) => s.type == "ship" && s.class == "container"),
+    "container.glb",
+    15,
+    0.1,
+    1
+  );
+  const military = gossipLayer(
+    actors.filter((s) => s.type == "ship" && s.class == "military"),
+    "military.glb",
+    15,
+    0.1,
+    1
+  );
 
-/*
+  /*
   other
   fishing
   special
@@ -31,7 +65,31 @@ export default function DeckGLLayers() {
   military
   */
 
-  const ships = gossipLayer(actors.filter((s) => s.type == "ship" && s.class != "navigation" && s.class != "tanker" && s.class != "cargo" && s.class != "container"), "duck.glb");
+  const ships = gossipLayer(
+    actors.filter(
+      (s) =>
+        s.type == "ship" &&
+        s.class != "navigation" &&
+        s.class != "tanker" &&
+        s.class != "cargo" &&
+        s.class != "container" &&
+        s.class != "military"
+    ),
+    "duck.glb"
+  );
 
-  return <DeckGLOverlay layers={[aircraft, navigation, tankers, cargo, container, ships]} interleaved={true}/>;
+  return (
+    <DeckGLOverlay
+      layers={[
+        aircraft,
+        navigation,
+        tankers,
+        cargo,
+        container,
+        military,
+        ships,
+      ]}
+      interleaved={true}
+    />
+  );
 }
