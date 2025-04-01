edda800ad5ae3681ad1cc313058a49cdf7bac918
Christian Panton <christian@panton.org>
2025-04-01 23:57:28 +0200

    reduce complexity of editing tiiks

--- src/features/map/geoman.ts
+++ src/features/map/geoman.ts
@@ -7,7 +7,29 @@ export const geomanOptions: GmOptionsPartial = {
   },
   controls: {
     draw: {
-        
+      rectangle: {
+        uiEnabled: false,
+      },
+    },
+    edit: {
+        cut: {
+            uiEnabled: false
+        },
+        rotate: {
+            uiEnabled: false
+        }
+    },
+    helper: {
+      snapping: {
+        active: true,
+        uiEnabled: false
+      },
+      zoom_to_features: {
+        uiEnabled: false
+      },
+      shape_markers: {
+        uiEnabled: false
+      }
     },
   },
 };
