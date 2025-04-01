de32c1d3a86a2cd17c2b9372d7e43e9182d5b1a3
Christian Panton <christian@panton.org>
2025-04-01 19:22:27 +0200

    starting light theme

--- src/components/layers/BaseLayers.tsx
+++ src/components/layers/BaseLayers.tsx
@@ -1,15 +1,27 @@
 import { Layer } from "react-map-gl/maplibre";
 
 import ToggleableLayer from "./shared/ToggleableLayer";
+import { useAppSelector } from "../../app/hooks";
+import { useMediaQuery } from "@mui/material";
 
 export default function BaseLayers() {
+  const { colorMode } = useAppSelector((state) => state.flags);
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const c = (light: string, dark: string): string => {
+    if (colorMode == "system") return prefersDarkMode ? dark : light;
+    return colorMode == "light" ? light : dark;
+  };
 
   return (
     <>
       <Layer
         type="background"
         id="background"
-        paint={{ "background-color": "#0e0e0e", "background-opacity": 1 }}
+        paint={{
+          "background-color": c("#fafaf8", "#0e0e0e"),
+          "background-opacity": 1,
+        }}
       />
 
       <ToggleableLayer
@@ -19,7 +31,7 @@ export default function BaseLayers() {
         source-layer="landcover"
         source="carto"
         paint={{
-          "fill-color": "#161b1f",
+          "fill-color": c("rgba(234, 241, 233, 0.5)", "#161b1f"),
           "fill-opacity": 1,
         }}
         filter={[
@@ -37,7 +49,7 @@ export default function BaseLayers() {
         source-layer="landcover"
         source="carto"
         paint={{
-          "fill-color": "#21282d",
+          "fill-color": c("#fff", "#21282d"),
           "fill-opacity": 1,
         }}
         filter={["==", "class", "ice"]}
@@ -54,13 +66,13 @@ export default function BaseLayers() {
           "fill-color": {
             type: "interval",
             stops: [
-              [5, "rgba(0, 0, 0, 0.5)"],
-              [8, "rgba(0, 0, 0, 0.45)"],
-              [9, "rgba(0, 0, 0, 0.4)"],
-              [11, "rgba(0, 0, 0, 0.35)"],
-              [13, "rgba(0, 0, 0, 0.3)"],
-              [15, "rgba(0, 0, 0, 0.25)"],
-              [16, "rgba(0, 0, 0, 0.15)"],
+              [5, c("rgba(237, 237, 237, 0.5)", "rgba(0, 0, 0, 0.5)")],
+              [8, c("rgba(237, 237, 237, 0.45)", "rgba(0, 0, 0, 0.45)")],
+              [9, c("rgba(237, 237, 237, 0.4)", "rgba(0, 0, 0, 0.4)")],
+              [11, c("rgba(237, 237, 237, 0.35)", "rgba(0, 0, 0, 0.35)")],
+              [13, c("rgba(237, 237, 237, 0.3)", "rgba(0, 0, 0, 0.3)")],
+              [15, c("rgba(237, 237, 237, 0.25)", "rgba(0, 0, 0, 0.25)")],
+              [16, c("rgba(237, 237, 237, 0.25)", "rgba(0, 0, 0, 0.15)")],
             ],
           },
           "fill-opacity": {
@@ -87,9 +99,9 @@ export default function BaseLayers() {
           "line-color": {
             type: "interval",
             stops: [
-              [4, "#222"],
-              [5, "#222"],
-              [6, "#2C353C"],
+              [4, c("#ead5d7", "#222")],
+              [5, c("#ead5d7", "#222")],
+              [6, c("#e1c5c7", "#2C353C")],
             ],
           },
           "line-width": {
@@ -121,9 +133,9 @@ export default function BaseLayers() {
           "line-color": {
             type: "interval",
             stops: [
-              [4, "rgba(103, 103, 114, 1)"],
-              [5, "rgba(103, 103, 114, 1)"],
-              [6, "rgba(103, 103, 114, 1)"],
+              [4, c("#ead5d7", "rgba(103, 103, 114, 1)")],
+              [5, c("#ead5d7", "rgba(103, 103, 114, 1)")],
+              [6, c("#e1c5c7", "rgba(103, 103, 114, 1)")],
             ],
           },
           "line-width": {
@@ -151,7 +163,7 @@ export default function BaseLayers() {
         source-layer="water"
         source="carto"
         paint={{
-          "fill-color": "#2C353C",
+          "fill-color": c("#d4dadc", "#2C353C"),
           "fill-antialias": true,
           "fill-translate-anchor": "map",
           "fill-opacity": 1,
@@ -164,7 +176,7 @@ export default function BaseLayers() {
         source-layer="waterway"
         source="carto"
         paint={{
-          "line-color": "rgba(63, 90, 109, 0.64)",
+          "line-color": c("#d1dbdf", "rgba(63, 90, 109, 0.64)"),
           "line-width": {
             type: "interval",
             stops: [
@@ -191,7 +203,6 @@ export default function BaseLayers() {
         paint={{ "raster-brightness-max": 0.8 }}
       ></ToggleableLayer>
 
-
       <ToggleableLayer
         group="borders"
         type="line"
@@ -203,7 +214,7 @@ export default function BaseLayers() {
         filter={["all", ["==", "admin_level", 2], ["==", "maritime", 0]]}
         layout={{ "line-cap": "round", "line-join": "round" }}
         paint={{
-          "line-color": "#2C353C",
+          "line-color": c("#f3efed", "#2C353C"),
           "line-opacity": 0.5,
           "line-width": 8,
           "line-offset": 0,
@@ -223,9 +234,9 @@ export default function BaseLayers() {
           "line-color": {
             type: "interval",
             stops: [
-              [4, "rgba(92, 94, 94, 1)"],
-              [5, "rgba(96, 96, 96, 1)"],
-              [6, "rgba(102, 102, 102, 1)"],
+              [4, c("#f2e6e7", "rgba(92, 94, 94, 1)")],
+              [5, c("#ebd6d8", "rgba(96, 96, 96, 1)")],
+              [6, c("#ebd6d8", "rgba(102, 102, 102, 1)")],
             ],
           },
           "line-width": {
--- src/components/layers/BoundariesLayers.tsx
+++ src/components/layers/BoundariesLayers.tsx
@@ -1,6 +1,16 @@
+import { useMediaQuery } from "@mui/material";
+import { useAppSelector } from "../../app/hooks";
 import ToggleableLayer from "./shared/ToggleableLayer";
 
 export default function BoundariesLayers() {
+  const { colorMode } = useAppSelector((state) => state.flags);
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const c = (light: string, dark: string): string => {
+    if (colorMode == "system") return prefersDarkMode ? dark : light;
+    return colorMode == "light" ? light : dark;
+  };
+
   return (
     <>
       <ToggleableLayer
@@ -13,7 +23,7 @@ export default function BoundariesLayers() {
         filter={["all", ["==", "admin_level", 2], ["==", "maritime", 1]]}
         layout={{ "line-cap": "round", "line-join": "round" }}
         paint={{
-          "line-color": "rgb(101, 101, 101)",
+          "line-color": c("#f3efed", "rgb(101, 101, 101)"),
           "line-width": {
             type: "interval",
             stops: [
@@ -30,7 +40,7 @@ export default function BoundariesLayers() {
         id="timezones"
         source="timezones"
         paint={{
-          "line-color": "rgb(50, 101, 101)",
+          "line-color": c("rgb(200, 200, 255)", "rgb(50, 101, 101)"),
           "line-width": {
             type: "interval",
             stops: [
